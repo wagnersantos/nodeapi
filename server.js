6a60cc8b4fa1ab58f960e9a953c434d41d5c765e
wagnersantos <wagtvc@gmail.com>
2019-03-05 10:50:00 -0300

    subindo api

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+//iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//iniciando o banco de dados
+mongoose.connect("mongodb://192.168.99.101:27017/nodeapi", {
+  useNewUrlParser: true
+});
+requireDir("./src/models");
+
+//rotas
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
